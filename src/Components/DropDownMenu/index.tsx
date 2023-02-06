37e8ecbbaafc4bfef8ed69f8a581d37a1fb83e90
F3540877 Gabriel Amorim Soares e Silva <amorimsoares@bb.com.br>
2023-02-06 17:50:30 -0300

    add project

---
+++ src/Components/DropDownMenu/index.tsx
@@ -0,0 +1,9 @@
+export default function DropDownMenu(){
+    return (
+        <div className="dropDown">
+            <h1>Vídeos</h1>
+            <h1>Contatos</h1>
+            <h1>Sobre</h1>
+        </div>
+    );
+}
--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,14 +1,26 @@
 import "./style.css"
 import { HiBars3 } from "react-icons/hi2"
+import { useState } from "react"
+import DropDownMenu from "../DropDownMenu";
 
 export function Header(){
+    const [dropShow, setDropShow] = useState(true);
+
     return (
         <header className="Header" >
             <img className="logo" src="../public/logoZeroVinte.jpg"></img>
             <h1>Zero Vinte</h1>
-            <button className="dropdownMenu" onClick={() => console.log("A")}>
+            <button className="dropDownBtn" onClick={() => setDropShow(!dropShow)}>
                 <HiBars3 id="menuIcon"/>
             </button>
+            { dropShow ? 
+            <div className="dropDownMenu">
+                <h1>Vídeos</h1>
+                <h1>Contatos</h1>
+                <h1>Sobre</h1> 
+            </div>
+                : <></>
+            }
         </header>
     )
 }
